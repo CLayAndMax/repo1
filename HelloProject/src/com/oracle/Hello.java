4ee6fff7b1e318535f59fb0f8dcf06a3b3f9a32d
CLayAdnMax <1679106670@qq.com>
2019-10-07 17:24:29 +0800

    添加一个Java项目

---
+++ HelloProject/src/com/oracle/Hello.java
@@ -0,0 +1,13 @@
+package com.oracle;
+
+/**
+ * @Auther: a
+ * @Date: 2019/10/7 17:14
+ * @Description:
+ */
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello");
+    }
+}
